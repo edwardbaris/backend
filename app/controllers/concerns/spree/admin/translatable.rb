40b06aeac7c8fae8782ed9ca1bc18c4935afa551
kpokorski <kfpokorski@gmail.com>
2023-08-28 15:44:02 +0200

    changing a way, the translations are saved

--- app/controllers/concerns/spree/admin/translatable.rb
+++ app/controllers/concerns/spree/admin/translatable.rb
@@ -14,8 +14,18 @@ module Spree
 
       def save_translation_values
         translation_params = params[:translation]
-
+        active_translations = Set.new
         current_store.supported_locales_list.each do |locale|
+          translation_params.each do |attribute, translations|
+            if !translations[locale].blank?
+              active_translations << locale
+            end
+          end
+        end
+
+        active_translations = active_translations.to_a
+
+        active_translations.each do |locale|
           translation = @object.translations.find_or_create_by(locale: locale)
           translation_params.each do |attribute, translations|
             translation.public_send("#{attribute}=", translations[locale])
